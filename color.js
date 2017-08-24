839be20dd037ba315b20179595c57630e7795af9
Daniel Lewis <daniellewis777@gmail.com>
2017-08-24 05:42:54 +0100

    Added color

--- color.js
+++ color.js
@@ -1,11 +1,15 @@
 'use strict'
 
 var chalk = require('chalk')
+var config = require('./config')
 
 function Color() {
 
   this.add = function(str, color) {
-    str = chalk[color](str)
+
+    /* istanbul ignore next */
+    if (!config.testing) str = chalk[color](str)
+
     return str
   }
 }
