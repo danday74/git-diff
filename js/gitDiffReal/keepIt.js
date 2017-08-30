aff4bfc8dfdecced4d20e2fd5ccf62b0e38a441f
Daniel Lewis <daniellewis777@gmail.com>
2017-08-30 01:09:21 +0100

    printf check to boolean

--- js/gitDiffReal/keepIt.js
+++ js/gitDiffReal/keepIt.js
@@ -4,16 +4,16 @@ var shell = require('shelljs')
 
 function isPrintf() {
   var printf = shell.which('printf')
-  return printf && printf.code === 0
+  return !!(printf && printf.code === 0)
 }
 
 function isGit() {
   var git = shell.which('git')
-  return git && git.code === 0
+  return !!(git && git.code === 0)
 }
 
 function isGitDir() {
-  return shell.exec('git rev-parse --is-inside-work-tree').code === 0
+  return shell.exec('git rev-parse --is-inside-work-tree', {silent: true}).code === 0
 }
 
 function KeepIt() {
