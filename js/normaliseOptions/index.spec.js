9cad6d88c81db4d627c1d036fca4eee7f353c321
Daniel Lewis <daniellewis777@gmail.com>
2017-08-30 22:45:47 +0100

    tested validate function

---
+++ js/normaliseOptions/index.spec.js
@@ -0,0 +1,71 @@
+'use strict'
+
+var DEFAULTS = require('../_shared/defaultOptions')
+var imp = require('../../test/_js/testImports')
+var normaliseOptions = require('./index')
+
+describe('normaliseOptions', function() {
+
+  var actual, expected
+
+  beforeEach(function() {
+    actual = undefined
+    expected = Object.assign({}, DEFAULTS)
+  })
+
+  it('options is undefined', function() {
+    actual = normaliseOptions()
+    imp.expect(actual).to.eql(expected)
+  })
+
+  it('options is not an object', function() {
+    actual = normaliseOptions(9)
+    imp.expect(actual).to.eql(expected)
+  })
+
+  it('color is a boolean', function() {
+    actual = normaliseOptions({color: true})
+    imp.expect(actual).to.eql(expected)
+  })
+
+  it('color is not a boolean', function() {
+    expected.color = false
+    actual = normaliseOptions({color: 'false'})
+    imp.expect(actual).to.eql(expected)
+  })
+
+  it('flags is a string', function() {
+    expected.flags = 'woteva'
+    actual = normaliseOptions({flags: ' woteva '})
+    imp.expect(actual).to.eql(expected)
+  })
+
+  it('flags is not a string', function() {
+    actual = normaliseOptions({flags: 9})
+    imp.expect(actual).to.eql(expected)
+  })
+
+  it('forceFake is a boolean', function() {
+    actual = normaliseOptions({forceFake: false})
+    imp.expect(actual).to.eql(expected)
+  })
+
+  it('forceFake is not a boolean', function() {
+    expected.forceFake = true
+    actual = normaliseOptions({forceFake: 9})
+    imp.expect(actual).to.eql(expected)
+  })
+
+  it('save', function() {
+    var SAVE = true
+    var WORD_DIFF = true
+    imp.expect(DEFAULTS.save).to.equal(false)
+    imp.expect(DEFAULTS.wordDiff).to.equal(false)
+    expected.save = SAVE
+    expected.wordDiff = WORD_DIFF
+    actual = normaliseOptions({save: SAVE, wordDiff: WORD_DIFF})
+    imp.expect(actual).to.eql(expected)
+    imp.expect(DEFAULTS.save).to.equal(false)
+    imp.expect(DEFAULTS.wordDiff).to.equal(WORD_DIFF)
+  })
+})
