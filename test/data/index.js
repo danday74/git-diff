29530c6d767d479258e4922d2b2b185a918863f7
Daniel Lewis <daniellewis777@gmail.com>
2017-09-25 02:25:25 +0100

    Updated deps

--- test/data/index.js
+++ test/data/index.js
@@ -1,21 +1,24 @@
 'use strict'
 
-var readfilego = require('readfilego')
+var readFileGo = require('readfile-go')
 
 module.exports = Object.freeze({
   endingsLinuxLineDiff: '-my first\n+my second\n string\n',
   endingsWindowsLineDiff: '-my first\r\n+my second\r\n string\r\n',
   endingsLinuxWordDiff: 'my [-first-]{+second+}\nstring\n',
   endingsWindowsWordDiff: 'my [-first-]{+second+}\r\nstring\r\n',
-  lineDiffFakeVim: readfilego(__dirname + '/lineDiffVim.txt', {throw: true, save: true}).replace(/^@@.+@@(?:\r?\n|\r)/, ''),
-  lineDiffRealVim: readfilego(__dirname + '/lineDiffVim.txt'),
+  lineDiffFakeVim: readFileGo(__dirname + '/lineDiffVim.txt', {
+    throw: true,
+    save: true
+  }).replace(/^@@.+@@(?:\r?\n|\r)/, ''),
+  lineDiffRealVim: readFileGo(__dirname + '/lineDiffVim.txt'),
   oneLinerLineDiffFake: '-my first string\n+my second string\n',
   oneLinerLineDiffReal: '@@ -1 +1 @@\n-my first string\n\\ No newline at end of file\n+my second string\n\\ No newline at end of file\n',
   oneLinerWordDiffFake: 'my [-first-]{+second+} string\n',
   oneLinerWordDiffReal: '@@ -1 +1 @@\nmy [-first-]{+second+} string\n',
-  shortstatReal: readfilego(__dirname + '/shortstatReal.txt'),
-  str1: readfilego(__dirname + '/str1.txt'),
-  str2: readfilego(__dirname + '/str2.txt'),
-  wordDiffFake: readfilego(__dirname + '/wordDiffFake.txt').replace(/^@@.+@@(?:\r?\n|\r)/, ''),
-  wordDiffReal: readfilego(__dirname + '/wordDiffReal.txt')
+  shortstatReal: readFileGo(__dirname + '/shortstatReal.txt'),
+  str1: readFileGo(__dirname + '/str1.txt'),
+  str2: readFileGo(__dirname + '/str2.txt'),
+  wordDiffFake: readFileGo(__dirname + '/wordDiffFake.txt').replace(/^@@.+@@(?:\r?\n|\r)/, ''),
+  wordDiffReal: readFileGo(__dirname + '/wordDiffReal.txt')
 })
