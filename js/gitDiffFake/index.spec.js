b9ac4d0598552afbd07787b59bd8a482bf9afd51
Daniel Lewis <daniellewis777@gmail.com>
2017-09-28 01:31:53 +0100

    Added more tests

---
+++ js/gitDiffFake/index.spec.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var gitDiffFake = require('./index')
+var imp = require('../../test/_js/testImports')
+
+describe('gitDiffFake', function() {
+
+  it('line difference', function() {
+    var expected = imp.data.lineDiffFakeVim
+    var actual = gitDiffFake(imp.data.str1, imp.data.str2, {color: false, wordDiff: false})
+    imp.expect(actual).to.equal(expected)
+  })
+
+  it('word difference', function() {
+    var expected = imp.data.wordDiffFake
+    var actual = gitDiffFake(imp.data.str1, imp.data.str2, {color: false, wordDiff: true})
+    imp.expect(actual).to.equal(expected)
+  })
+})
