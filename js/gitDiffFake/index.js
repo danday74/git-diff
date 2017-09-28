70388739849d41200183da2bded61cb7a4baeb49
Daniel Lewis <daniellewis777@gmail.com>
2017-09-28 20:31:26 +0100

    line endings

--- js/gitDiffFake/index.js
+++ js/gitDiffFake/index.js
@@ -4,7 +4,6 @@ var lineDiffFake = require('./lineDiffFake')
 var wordDiffFake = require('./wordDiffFake')
 
 function gitDiffFake(str1, str2, options) {
-
   return (options.wordDiff) ? wordDiffFake(str1, str2, options) : lineDiffFake(str1, str2, options)
 }
 
