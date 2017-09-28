7658c7426b9f323021d81c425e3f9d177b85c871
Daniel Lewis <daniellewis777@gmail.com>
2017-09-28 20:23:53 +0100

    Whitespace

--- js/gitDiffFake/color.js
+++ js/gitDiffFake/color.js
@@ -4,12 +4,9 @@ var chalk = require('chalk')
 var config = require('../../config')
 
 function Color() {
-
   this.add = function(str, color) {
-
     /* istanbul ignore next */
     if (!config.testing) str = chalk[color](str)
-
     return str
   }
 }
